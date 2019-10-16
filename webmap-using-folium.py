5479f594ba2f064396393a364941cf6142c8d947
Shashi Sourav <31927041+sourav970@users.noreply.github.com>
2019-10-16 21:52:00 +0530

    Create webmap-using-folium.py
    Webmap-using-folium created and code is added.

---
+++ webmap-using-folium.py
@@ -0,0 +1,17 @@
+import folium
+# create feature group
+
+##parent of feture group
+fg=folium.FeatureGroup("My Map")
+
+##child
+fg.add_child(folium.GeoJson(data=(open('india_states.json','r',encoding='utf-8-sig').read())))
+
+fg.add_child(folium.Marker(location=[18.5204,73.8567],popup="Taj Mahal hai lavde"))
+
+map=folium.Map(Location=[27.1751,78.0421],zoom_start=5)
+
+map.add_child(fg)
+map.save("test.html")
+             
+
