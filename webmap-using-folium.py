fcd4cb8893f3205e3c76011d467cfcf483e38ac5
Mohsinraza1052 <56648670+Mohsinraza1052@users.noreply.github.com>
2019-10-16 22:44:01 +0530

    Update webmap-using-folium.py
    Correct message

--- webmap-using-folium.py
+++ webmap-using-folium.py
@@ -7,7 +7,7 @@ fg=folium.FeatureGroup("My Map")
 ##child
 fg.add_child(folium.GeoJson(data=(open('india_states.json','r',encoding='utf-8-sig').read())))
 
-fg.add_child(folium.Marker(location=[18.5204,73.8567],popup="Taj Mahal hai lavde"))
+fg.add_child(folium.Marker(location=[18.5204,73.8567],popup="it's Taj mahal"))
 
 map=folium.Map(Location=[27.1751,78.0421],zoom_start=5)
 
